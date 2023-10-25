e373214e859cde5e817b8b926f0520e872e479ee
Binyam Minybel <63998876+Binyamkpaul@users.noreply.github.com>
2023-10-25 09:59:21 +0300

    nav bar sm device resp

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,49 +1,40 @@
 import Link from "next/link";
 import Image from "next/image";
-// ... (other imports)
 
 const Nav = () => {
-  // ... (other code)
-
   return (
-    <>
-      <nav className="flex fixed top-0 left-0 right-0 bg-gray-100 md:bg-lime-500 text-white glassmorphism md:glassmorphism-lime justify-between items-center h-20 z-50 shadow-sm">
-        {/* LOGO Section */}
-        <section className="flex justify-between items-center pl-3 md:pl-0 h-full">
-          <div className="w-28 md:w-64 bg-transparent md:bg-white flex justify-center items-center rounded-tr-3xl h-full">
-            <Link href="">
-              <Image
-                src="https://developer.ethiotelecom.et/assets/images/et-logo-2.svg"
-                alt="logo"
-                width={160}
-                height={150}
-                className="object-contain"
-              />
-            </Link>
-          </div>
+    <nav className="flex fixed top-0 left-0 right-0 bg-white md:bg-lime-500 text-white glassmorphism md:glassmorphism-lime justify-between items-center h-20 z-50 shadow-sm">
+      {/* LOGO Section */}
+      <section className="bg-white rounded-tr-3xl flex justify-center items-center h-full w-1/3 md:w-1/4">
+        <Link href="">
+          <Image
+            src="https://developer.ethiotelecom.et/assets/images/et-logo-2.svg"
+            alt="logo"
+            width={160}
+            height={150}
+            className="object-contain"
+          />
+        </Link>
+      </section>
 
-          {/* Add the first image */}
-        </section>
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden w-full justify-between items-center">
+        {/* ... (other code) */}
+      </div>
 
-        {/* Desktop Navigation */}
-        <div className="sm:flex hidden w-full justify-between items-center">
-          {/* ... (other code) */}
-        </div>
-
-        {/* Mobile Navigation */}
-        <section className="bg-white rounded-tl-3xl flex justify-center items-center h-full w-80 ">
-          <Link href="">
-            <Image
-              src="https://developer.ethiotelecom.et/assets/images/telebirr-logo-color.svg"
-              alt="logo"
-              width={160}
-              height={150}
-              className="object-contain"
-            />
-          </Link>
-        </section>
-      </nav>
-    </>
+      {/* Mobile Navigation */}
+      <section className="bg-white rounded-tl-3xl flex justify-center items-center h-full w-1/3 md:w-1/4">
+        <Link href="">
+          <Image
+            src="https://developer.ethiotelecom.et/assets/images/telebirr-logo-color.svg"
+            alt="logo"
+            width={160}
+            height={150}
+            className="object-contain"
+          />
+        </Link>
+      </section>
+    </nav>
   );
 };
 
